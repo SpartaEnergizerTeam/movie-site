697322a8e6e486c9240b503594359b8b19e71669
LinQuixote <leein9404@naver.com>
2024-05-02 17:39:47 +0900

    refactor : return, url 상수화, json 수정,불필요  주석제거 유툽 비디오주소 리턴  함수 작성

--- js/api.js
+++ js/api.js
@@ -1,4 +1,3 @@
-// 옵션
 const options = {
   method: "GET",
   headers: {
@@ -7,18 +6,27 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTQ5MzRmNzg4ZjRlYzI1NmViY2RlYzcyNzVhMDdlZCIsInN1YiI6IjY2MjdhNGMyNjJmMzM1MDE2NGQ5ZDU0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UrmYmAFfVZN-YSlLPyTfP10QreSXWz1VYKRQeak1yxU",
   },
 };
+const defaultUrl = "https://api.themoviedb.org/3/";
 
-const urlAddress = {
-  playing: "https://api.themoviedb.org/3/movie/now_playing?language=ko&page=2",
-  popular: "https://api.themoviedb.org/3/movie/popular?language=ko&page=1",
-  top: "https://api.themoviedb.org/3/movie/top_rated?language=ko&page=3",
-  upcoming: "https://api.themoviedb.org/3/movie/upcoming?language=ko&page=4",
-};
-// 아래의 함수는 위의 urlAddress 안에서 하나씩 url 꺼내서 사용하시면 됩니다.
-export const getData = async (url) => {
+// playing, popular, top, upcoming 중 하나를 movieTypeString의 인자로 넣으면 됩니다.
+export const getData = async (movieTypeString, pageNum) => {
+  let url = "";
+  switch (movieTypeString) {
+    case "playing":
+      url = `${defaultUrl}now_playing?language=ko&page=${pageNum}`;
+      break;
+    case "popular":
+      url = `${defaultUrl}popular?language=ko&page=${pageNum}"`;
+      break;
+    case "top":
+      url = `${defaultUrl}top_rated?language=ko&page=${pageNum}`;
+      break;
+    case "upcoming":
+      url = `${defaultUrl}upcoming?language=ko&page=${pageNum}`;
+      break;
+  }
   const response = await fetch(url, options);
-  const data = await response.json(response);
-  return data;
+  return await response.json();
 };
 
 // 영화를 한글 키워드로 검색을 할 수 있습니다.
@@ -29,23 +37,36 @@ export const getMovieWithKeywords = async (keywords) => {
     `https://api.themoviedb.org/3/search/movie?query=${urlString}&include_adult=false&language=ko&page=1`,
     options
   );
-  const data = await response.json(response);
-  return data;
+  return await response.json();
 };
 
 // 무비 id를 입력하면 영화 디테일 정보를 가져옵니다.
 export const getMovieDetail = async (movie_id) => {
-  const url = `https://api.themoviedb.org/3/movie/${movie_id}?language=ko`;
+  const url = `${defaultUrl}${movie_id}?language=ko`;
   const response = await fetch(url, options);
-  const data = await response.json();
-
-  return data;
+  return await response.json();
 };
 
 // 무비 id를 입력하면 영화 이미지들을 가져옵니다.
 export const getMovieImages = async (movie_id) => {
-  const url = `https://api.themoviedb.org/3/movie/${movie_id}/images`;
+  const url = `${defaultUrl}${movie_id}/images`;
+  const response = await fetch(url, options);
+  return await response.json();
+};
+
+// 무비 id를 입력하면 영화 영상들을 유튜브로 가져옵니다.
+// 유튜브 url을 리턴하는 함수로 없으면 빈배열을 리턴합니다.
+export const getMovieVideos = async (movie_id) => {
+  const url = `${defaultUrl}${movie_id}/videos?language=ko`;
   const response = await fetch(url, options);
   const data = await response.json();
-  return data;
+
+  // 하나라도 있으면?
+  if (data.results.length !== 0) {
+    const videoList = data.results.map((value) => {
+      return `https://www.youtube.com/watch?v=${value.key}`;
+    });
+    return videoList;
+  }
+  return [];
 };
