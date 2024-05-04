2ccf18495514310bb960940b3e79fbfa813fc104
LinQuixote <leein9404@naver.com>
2024-05-04 19:59:06 +0900

    feat : 페이지 이동

--- js/profile.js
+++ js/profile.js
@@ -4,7 +4,7 @@
 // animate__zoomInDown
 
 var disabled = false;
-const returnUrl = "/index.html";
+const returnUrl = "index.html";
 let imageUrl =
   "https://t4.ftcdn.net/jpg/00/64/67/27/240_F_64672736_U5kpdGs9keUll8CRQ3p3YaEv2M6qkVY5.jpg";
 
