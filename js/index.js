5f7e60af9c950ba6381572a2dead2237cc883b23
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-05-04 23:59:51 +0900

    style: 코드 수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-// 슬라이드 배너 함수
+import { getMovieData } from "./api.js";
 
+// 슬라이드 배너 함수
 function SliderBox1__init() {
   const swiper = new Swiper("#main-banner", {
     // Optional parameters
@@ -21,24 +22,19 @@ function SliderBox1__init() {
 SliderBox1__init();
 
 // 영화 api data 출력
-
-import { getMovieData } from "./api.js";
-
 const loadMoviesAndDisplay = async () => {
   try {
     const movieData = await getMovieData("top");
     const movies = movieData.results;
 
     const cardList = document.querySelector("#cards");
-    cardList.innerHTML = "";
 
     const cardsHtml = movies
-      .map(
-        (movie) =>
-          `<li class="card-poster" id=${movie.id}>
+      .map((movie) => {
+          return `<li class="card-poster" id=${movie.id}>
           <img class="card-img" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}"/>
          </li>`
-      )
+        })
       .join("");
 
     cardList.innerHTML = cardsHtml;
