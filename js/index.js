d4b8a436f58ee2c189383ec83e17c00f9ae1df3c
yeeunseo-dev <anfrhrl96@gmail.com>
2024-05-05 02:34:50 +0900

    feat: 영화 전체데이터연결  및 캐러셀

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 import { getMovieData } from "./api.js";
 
 // 슬라이드 배너 함수
+// 3번 이미지부터 뜨는 것, 옆 이미지가 안뜨는 것 해결해야함 //
+
 function SliderBox1__init() {
   new Swiper("#main-banner", {
     loop: true, //반복재생
-    slidesPerView: 'auto', // 슬라이드 갯수 조정 (지금은 css로 맞춰두었기 때문에  auto로 함)
+    slidesPerView: "auto", // 슬라이드 갯수 조정 (지금은 css로 맞춰두었기 때문에  auto로 함)
     spaceBetween: 20, // 다른 슬라이드와의 간격
     centeredSlides: true, // 가운데 중심의 슬라이드
     autoplay: {
-      delay: 5000, // 5초마다 자동 재생
+      delay: 3000, // 3초마다 자동 재생
     },
     disableOnInteraction: false, // 사람이 넘겨도 멈추지 않고 다시 자동재생 유지
     pagination: {
@@ -26,25 +28,48 @@ function SliderBox1__init() {
 SliderBox1__init();
 
 // 영화 api data 출력
-const loadMoviesAndDisplay = async () => {
+const loadMoviesAndDisplay = async (movieTypeString) => {
   try {
-    const movieData = await getMovieData("top");
+    const movieData = await getMovieData(movieTypeString);
     const movies = movieData.results;
 
-    const cardList = document.querySelector("#cards");
+    const cardList = document.querySelector(`#cards-${movieTypeString}`);
 
     const cardsHtml = movies
       .map((movie) => {
-          return `<li class="card-poster" id=${movie.id}>
+        return `<li class="card-poster swiper-slide" id=${movie.id}>
           <img class="card-img" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}"/>
-         </li>`
-        })
+          </li>`;
+      })
       .join("");
 
     cardList.innerHTML = cardsHtml;
   } catch (error) {
-    console.error("영화 데이터를 불러오는 중 오류가 발생했습니다:", error);
+    console.error(
+      "영화 데이터(${movieTypeString})를 불러오는 중 오류가 발생했습니다:",
+      error
+    );
   }
 };
 
-loadMoviesAndDisplay();
+loadMoviesAndDisplay("top");
+loadMoviesAndDisplay("playing");
+loadMoviesAndDisplay("upcoming");
+loadMoviesAndDisplay("popular");
+
+// 영화리스트 스크롤
+document.addEventListener("DOMContentLoaded", function () {
+  movieScroll();
+});
+
+function movieScroll() {
+  new Swiper(`#card-carousel`, {
+    slidesPerView: 5,
+    paginationClickable: true,
+    spaceBetween: 5,
+    freeMode: false,
+    slidesPerGroup: 5,
+  });
+}
+
+movieScroll();
